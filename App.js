3adbc3ce7ae6aa55e589f807249ccc4630e8e011
Hyesu <hesushim@gmail.com>
2022-06-29 00:12:11 +0900

    프로젝트 준비, 라이브러리 설치

--- App.js
+++ App.js
@@ -1,24 +1,14 @@
-import React from "react";
-import { Text, View } from "react-native";
-//react-native의 SafeAreaView 컴포넌트는 상단과 하단에 여백을 설정해 안전한 영역에만 UI콘텐츠 보이도록 제한
-//react-native-safe-area-context: 특정부분 여백 비활성화
-import {
-  SafeAreaProvider,
-  SafeAreaView,
-  useSafeAreaInsets,
-} from "react-native-safe-area-context";
+import * as React from "react";
+import { View, Text, StyleSheet } from "react-native";
 
 export default function App() {
   return (
-    <SafeAreaProvider>
-      {/*
-      edges Props: safeArea 적용할 모서리 
-      겹쳐진 영역 수정하려면 Status 높이와 일치하는 빈View 보여주면 됨
-      StatusBar의 높이 -> useSafeAreaInsets Hook함수
-      */}
-      <SafeAreaView edges={["bottom"]}>
-        <Text>안녕</Text>
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <View style={styles.container}>
+      <Text>안녕</Text>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: { flex: 1 },
+});
