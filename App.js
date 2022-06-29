a0c5cdf9407f818bdd7ef04a7d943dacdfbf4513
Hyesu <hesushim@gmail.com>
2022-06-29 23:05:57 +0900

    Context API사용하기_예제

--- App.js
+++ App.js
@@ -2,11 +2,17 @@ import React from "react";
 import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import RootStack from "./screens/RootStack";
+import LogContext from "./contexts/LogContext";
 
 export default function App() {
   return (
     <NavigationContainer>
-      <RootStack />
+      {/* Provider에서 value라는 Props 설정 
+      -> Context를 통해 여러 컴포넌트에서 공유 
+      -> 이 컴포넌트 내부에 선언된 모든 컴포넌트에서 Context 값 사용 가능*/}
+      <LogContext.Provider value="안녕하세요">
+        <RootStack />
+      </LogContext.Provider>
     </NavigationContainer>
   );
 }
--- screens/FeedsScreen.js
+++ screens/FeedsScreen.js
@@ -1,10 +1,15 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import LogContext from "../contexts/LogContext";
 
 function FeedsScreen() {
   return (
     <View>
-      <Text>작성한 글 목록 형태로 보여주는 화면</Text>
+      {/* Context 안의 값 사용 -> Consumer컴포넌트 사용 */}
+      <LogContext.Consumer>
+        {/* Render Props 패턴: 컴포넌트 태그 사이에 함수 */}
+        {(value) => <Text>{value}</Text>}
+      </LogContext.Consumer>
     </View>
   );
 }
