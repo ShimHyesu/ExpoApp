9751bd7353557f1b943efa15aba6d67304fb8ff3
Hyesu <hesushim@gmail.com>
2022-07-01 21:46:40 +0900

    Context API_children Props

--- screens/FeedsScreen.js
+++ screens/FeedsScreen.js
@@ -5,15 +5,31 @@ import LogContext from "../contexts/LogContext";
 function FeedsScreen() {
   return (
     <View>
-      {/* Context 안의 값 사용 -> Consumer컴포넌트 사용 */}
-      <LogContext.Consumer>
-        {/* Render Props 패턴: 컴포넌트 태그 사이에 함수 */}
-        {(value) => <Text>{value}</Text>}
-      </LogContext.Consumer>
+      {/* 
+      <Box>
+        <Text>1</Text>
+      </Box>
+      */}
+      <Box>{(value) => <Text>{value}</Text>}</Box>
     </View>
   );
 }
 
 export default FeedsScreen;
 
-const styles = StyleSheet.create({});
+function Box({ children }) {
+  // Box컴포넌트 태그 사이에 넣은 JSX를 children이라는 Props로 받아와 사용 가능
+  //return <View style={styles.box}>{children}</View>;
+
+  //Hello World라는 값을 FeedsScreen 컴포넌트에 전달하여 사용하도록 만드는 방법
+  return <View style={styles.box}>{children("Hello World")}</View>;
+}
+
+const styles = StyleSheet.create({
+  box: {
+    borderWidth: 2,
+    padding: 16,
+    borderBottomColor: "black",
+    marginBottom: 16,
+  },
+});
