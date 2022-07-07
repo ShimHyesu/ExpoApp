ee8e323208dee23a0ed6f1a18996a8282f8babaa
Hyesu <hesushim@gmail.com>
2022-07-08 00:08:35 +0900

    검색 기능 - 검색어 필터링 후 FeedList 재사용

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,13 +1,28 @@
 import React, { useContext } from "react";
 import { View, Text, StyleSheet } from "react-native";
-import SearchContext from "../contexts/SearchContext";
 
-function SearchScreen() {
-  const { keyword, onChangeText } = useContext(SearchContext);
+import SearchContext from "../contexts/SearchContext";
+import LogContext from "../contexts/LogContext";
+// 재사용
+import FeedList from "../components/FeedList";
+
+function SearchScreen({ navigation }) {
+  const { keyword } = useContext(SearchContext);
+  const { logs } = useContext(LogContext);
+
+  const filtered =
+    // 검색어 있을때 필터링, 없을때 빈 배열
+    keyword === ""
+      ? []
+      : logs.filter((log) =>
+          // some 배열 내장 함수 : 배열 원소 중 특정 조건이 true인 원소 하나라도 있으면 true, 모두 아니면 false
+          // text.includes 문자열 내장 함수 : 텍스트에 특정 문자열이 존재하는지 확인 -> true/false
+          [log.title, log.body].some((text) => text.includes(keyword))
+        );
 
   return (
     <View style={styles.block}>
-      <Text>{keyword}</Text>
+      <FeedList logs={filtered} />
     </View>
   );
 }
@@ -15,5 +30,7 @@ function SearchScreen() {
 export default SearchScreen;
 
 const styles = StyleSheet.create({
-  block: {},
+  block: {
+    flex: 1,
+  },
 });
