479a3fd5d9ec44c118d56d46bd447d0669729368
Hyesu <hesushim@gmail.com>
2022-07-21 21:50:22 +0900

    달력 기능 구현하기 - useMemo Hook으로 최적화

--- screens/CalendarScreen.js
+++ screens/CalendarScreen.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useMemo } from "react";
 import { StyleSheet } from "react-native";
 import { format } from "date-fns";
 
@@ -13,14 +13,18 @@ function CalendarScreen() {
     format(new Date(), "yyyy-MM-dd")
   );
 
-  // logs 배열을 Calendar 컴포넌트의 markedDates Prop형태로 변환
-  // reduce함수: 배열 내장 함수 -> 배열 안 값 연산하여 하나의 결과값 도출
-  // reduce 첫번째 파라미터: 배열 각 우너소 특정값 연산하는 함수, 두번째 파라미터: 초기값
-  const markedDates = logs.reduce((acc, current) => {
-    const formattedDate = format(new Date(current.date), "yyyy-MM-dd");
-    acc[formattedDate] = { marked: true };
-    return acc;
-  }, {});
+  // markedDates는 날짜가 바뀌어도 변하지 않기 때문에 매번 리렌더링 될 필요x
+  // useMemo Hook 사용하여 이전 계산한 값 재사용하여 처리 최적화
+  // logs 배열 바뀔때만 logs.reduce함수 수행
+  const markedDates = useMemo(
+    () =>
+      logs.reduce((acc, current) => {
+        const formattedDate = format(new Date(current.date), "yyyy-MM-dd");
+        acc[formattedDate] = { marked: true };
+        return acc;
+      }, {}),
+    [logs]
+  );
 
   // 현재 선택된 날짜로 로그를 필터링하여 FeedList에 전달하고 ListHeaderComponent에는 CalendarView 설정
   const filteredLogs = logs.filter(
